d9e516902c7f027a63db322e6b78a2d26c0d5282
MMZhirkov <mmzhirkov@yandex.ru>
2017-05-14 02:57:08 +0300

    Send email + attachment ready

---
+++ Send Mail/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace NetConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SendEmailAsync().GetAwaiter();
+            Console.Read();
+        }
+
+        private static async Task SendEmailAsync()
+        {
+
+            MailAddress from = new MailAddress("zhirkovmichael@gmail.com", "Michael Zhirkov");
+            MailAddress to = new MailAddress("mmzhirkov@yandex.ru");
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = "Тест";
+            m.Body = "Письмо-тест работы smtp-клиента";
+            m.Attachments.Add(new Attachment("E://test.txt"));
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential("@gmail.com", "Pas");
+            smtp.EnableSsl = true;
+            await smtp.SendMailAsync(m);
+            Console.WriteLine("Письмо отправлено");
+            Console.ReadKey();
+        }
+    }
+}
