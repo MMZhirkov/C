91c30dffa015b77bb884e1aacbf716ec37455839
MMZhirkov <mmzhirkov@yandex.ru>
2017-05-15 05:23:39 +0300

    ready login form

---
+++ login form/login form/login form/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "User1" && textBox2.Text == "123")
+            {
+                Form2 s = new Form2();
+                s.Show();
+
+                this.Hide();
+            }
+            else
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                MessageBox.Show("Попробуйте еще");
+            }
+        }
+    }
+}
