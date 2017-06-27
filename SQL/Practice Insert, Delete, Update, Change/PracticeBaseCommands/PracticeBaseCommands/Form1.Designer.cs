7948da40753f27d226b272a2a481b39c7d7d79ff
MMZhirkov <mmzhirkov@yandex.ru>
2017-06-27 22:15:33 +0300

    add readme to Sql

--- SQL/Practice Insert, Delete, Update, Change/PracticeBaseCommands/PracticeBaseCommands/Form1.Designer.cs
+++ SQL/Practice Insert, Delete, Update, Change/PracticeBaseCommands/PracticeBaseCommands/Form1.Designer.cs
@@ -257,6 +257,7 @@
             this.button2.TabIndex = 13;
             this.button2.Text = "CHANGE";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label4
             // 
@@ -340,6 +341,7 @@
             this.button3.TabIndex = 20;
             this.button3.Text = "DELETE";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label9
             // 
--- SQL/Practice Insert, Delete, Update, Change/PracticeBaseCommands/PracticeBaseCommands/Form1.cs
+++ SQL/Practice Insert, Delete, Update, Change/PracticeBaseCommands/PracticeBaseCommands/Form1.cs
@@ -13,7 +13,7 @@ namespace PracticeBaseCommands
 {
     public partial class Form1 : Form
     {
-        SqlConnection sqlConnection;//объявляем поля класса для доступа к его объекту в форме
+        SqlConnection sqlConnection;//объявляем поле класса для доступа к его объекту в форме
         
         public Form1()
         {
@@ -21,7 +21,7 @@ namespace PracticeBaseCommands
         }
         private async void Form1_Load(object sender, EventArgs e)
         {
-            //получаем физ расположение нашей базы//
+            //получаем физ расположение БД//
             string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\c#\SQL\Practice Insert, Delete, Update, Change\PracticeBaseCommands\PracticeBaseCommands\DatabaseMagazine.mdf;Integrated Security=True";
             sqlConnection = new SqlConnection(connectionString);
 
@@ -107,6 +107,44 @@ namespace PracticeBaseCommands
             }
         }
         //CHANGE//
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (label8.Visible)
+                label8.Visible = false;
+            if (!string.IsNullOrEmpty(textBox4.Text) && !string.IsNullOrWhiteSpace(textBox4.Text) &&
+                !string.IsNullOrEmpty(textBox5.Text) && !string.IsNullOrWhiteSpace(textBox5.Text)&&
+                !string.IsNullOrEmpty(textBox6.Text) && !string.IsNullOrWhiteSpace(textBox6.Text))
+            {
+                SqlCommand command = new SqlCommand("UPDATE [Products]SET [Name]=@Name, [Price]=@Price WHERE [Id]=@Id", sqlConnection);
+                command.Parameters.AddWithValue("Id", textBox6.Text);
+                command.Parameters.AddWithValue("Name", textBox4.Text);
+                command.Parameters.AddWithValue("Price", textBox5.Text);
+                await command.ExecuteNonQueryAsync();
+            }
+            else
+            {
+                label8.Visible = true;
+                label8.Text = "Все поля должны быть заполнены";
+            }
+        }
         //DELETE//
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            if (label10.Visible)
+                label10.Visible = false;
+            if (!string.IsNullOrEmpty(textBox9.Text) && !string.IsNullOrWhiteSpace(textBox9.Text))
+            {
+                SqlCommand command = new SqlCommand("DELETE FROM [Products] WHERE [Id]=@Id", sqlConnection);
+                command.Parameters.AddWithValue("Id", textBox9.Text);
+               
+
+                await command.ExecuteNonQueryAsync();
+            }
+            else
+            {
+                label10.Visible = true;
+                label10.Text = "Все поля должны быть заполнены";
+            }
+        }
     }
 }
