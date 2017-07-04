43ec9b751a3a84f84f49bf278cce620c8836a7d9
MMZhirkov <mmzhirkov@yandex.ru>
2017-07-04 17:45:12 +0300

    add dynamic array

---
+++ Draft/ParserHTML/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Dynamic array";
+
+            int n;//number
+
+            Console.Write("ENTER THE NUMBER OF ELEMENTS");
+            n = int.Parse(Console.ReadLine());
+            //initialize array
+            int[] a = new int[n];
+
+            Console.WriteLine("ENTER ELEMENTS OF ARRAY");
+            for (int i=0;i<n;i++)
+            {
+                Console.Write("ENTER ELEMENT A ["+i+"]: ");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i=0; i < a.Length; i++)
+            {
+                Console.Write(a[i] +" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
