372109cb729a376d921dfe1b618084a542e6b6fd
MMZhirkov <mmzhirkov@yandex.ru>
2017-07-26 11:28:00 +0300

    add first task for mindbox

---
+++ Draft/virtualoverride/ConsoleApp1/ConsoleApp1/AdHoc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DerivedClass:BaseClass
+    {
+        public void Method()
+        {
+            base.Method();
+            Console.WriteLine("Method from DerivedClass");
+        }
+    }
+}
---
+++ MindBox/First Task/AreaTriangleLib/AreaTriangleLib/MyArea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTriangleLib
+{
+    public class MyArea 
+    {
+    public int Area(int bottom, int height)
+    {
+            return bottom * height / 2;
+    }
+    }
+}
