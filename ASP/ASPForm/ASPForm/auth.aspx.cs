af22a8f810a39b595f11fd5cc7f98571af8cb531
MMZhirkov <mmzhirkov@yandex.ru>
2017-08-10 20:45:04 +0300

    add asp form authorization

---
+++ ASP/ASPForm/ASPForm/auth.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPForm
+{
+    public partial class auth : System.Web.UI.Page
+    {
+        String user = "User1";
+        String password = "1234";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text==user)
+            {
+                if (TextBox2.Text==password)
+                {
+                    Response.Redirect("/home.aspx");
+                }
+                else
+                {
+                    Label3.Text = "Password incorrect";
+                }
+                Response.Redirect("/home.aspx");
+            }
+            else
+            {
+                Label3.Text = "Login incorrect";
+            }
+        }
+    }
+}
---
+++ EasyPrograms/Sorting2/Sorting/Sorting/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Bubble sort
+            ////enter numbers
+            //int[] nums = new int[7];
+            //Console.WriteLine("Введите 7 чисел");
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    Console.WriteLine("{0}-е число: ", i + 1);
+            //    nums[i] = Int32.Parse(Console.ReadLine());
+            //}
+            ////sort
+            //int temp;
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    for (int j = i+1; j < nums.Length; j++)
+            //    {
+            //        if (nums[i]>nums[j])
+            //        {
+            //             temp = nums[i];
+            //            nums[i] = nums[j];
+            //            nums[j] = temp;
+            //        }
+            //    }
+            //}
+            #endregion
+            #region Find premax element
+            int[] nums = new int[7];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine("Enter {0} -number", i);
+                nums[i] = Int32.Parse(Console.ReadLine());
+            }
+            int max;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i]>nums[j])
+                    {
+                        max = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = max;
+                    }                    
+                }
+            }
+            int k = nums.Length-1;
+            Console.WriteLine(nums[k]);
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
