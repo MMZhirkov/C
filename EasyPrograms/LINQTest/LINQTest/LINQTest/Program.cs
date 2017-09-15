1782666eb2286ea181bdbff8a6da266e363e37b6
MMZhirkov <mmzhirkov@yandex.ru>
2017-09-15 18:35:37 +0300

    add bot for telegram v 05

---
+++ EasyPrograms/LINQTest/LINQTest/LINQTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQTest
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string[] names = { "Tom", "Dick", "Harry", "Mary", "Michael", };
+            var list = new List<string>();
+            //foreach (var name in names)
+            //{
+            //    if (name.Contains("r"))
+            //    {
+            //        list.Add(name.ToUpper());
+            //    }
+            //}
+            var query = names.Where(x => x.Contains("r")).Select(x=>x.ToUpper());
+          
+                Console.WriteLine(names.Where(x => x.Contains("r")).Select(x => x.ToUpper()));
+           
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TelegramBots/TelegramBotApp/TelegramBotApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="TelegramBotApp.MvcApplication" Language="VB" %>
