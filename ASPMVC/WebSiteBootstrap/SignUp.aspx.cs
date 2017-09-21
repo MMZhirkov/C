fb850bb5d5ade4c063b0e4b6439997e7b2fbc0bc
MMZhirkov <mmzhirkov@yandex.ru>
2017-09-21 22:41:35 +0300

    add HAP Practice easy examle and bigen work with bootstrap&asp.net

---
+++ ASPMVC/WebSiteBootstrap/SignUp.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public partial class SignUp : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btSignup_Click(object sender, EventArgs e)
+    {
+        String CS = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString1"].ConnectionString;
+        using(SqlConnection con= new SqlConnection(CS))
+        {
+            SqlCommand cmd = new SqlCommand("insert into Users values('"+tbUname.Text+ "','" + tbPass.Text + "','" + tbEmail.Text + "','" + tbName.Text + "')", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
