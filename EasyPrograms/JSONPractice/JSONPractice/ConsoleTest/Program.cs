76df33b3d43b20324c3cc41b7a9507daf2e141e1
MMZhirkov <zhirkovmichael@gmail.com>
2017-10-13 23:10:27 +0300

    add json example1(practice serialize and desearialize)

---
+++ EasyPrograms/JSONPractice/JSONPractice/ConsoleTest/Program.cs
@@ -0,0 +1,27 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var person = new Person
+            {
+                Age = 19,
+                FirstName = "Andrew",
+                LastName = "Men"
+            };
+            string jsonData = JsonConvert.SerializeObject(person);
+            var person2 = JsonConvert.DeserializeObject<Person>(jsonData);
+
+            Console.ReadKey();
+        }
+    }
+}
