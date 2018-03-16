1cb5c51c6e07fc68420d5882589fe3ad574de4de
MMZhirkov <mmzhirkov@yandex.ru>
2018-03-16 18:58:26 +0300

    add list tags

--- InstaBot/InstaBot/InstaBot/Form1.cs
+++ InstaBot/InstaBot/InstaBot/Form1.cs
@@ -10,41 +10,54 @@ using System.Windows.Forms;
 using OpenQA.Selenium;
 using NLog; 
 using NLog.Config;
+using System.Threading;
 
 namespace InstaBot
 {
     public partial class Form1 : Form
     {
         private static Logger logger = LogManager.GetCurrentClassLogger();
+        List<string> Tags = new List<string>();
+        
+
+
         IWebDriver Browser;
         public Form1()
         {
             InitializeComponent();
-            logger.Trace("log {0}", this.Text);
+            Login();
+
+
+            Tags.Add("2");
+            //revision
+            //1 work for tag
+            //2 like menee 200
+            //3 video prosmotr
+        }
+        public void Login() {
+            string currentUrl;
             //pass authorization
+            logger.Info("login");
             Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
             Browser.Navigate().GoToUrl("https://www.instagram.com/accounts/login/");
             IWebElement SearchLogin = Browser.FindElement(By.Name("username"));
-            SearchLogin.SendKeys("wowbow@inbox.ru");
+            SearchLogin.SendKeys("mmzhirkov@yandex.ru");
             IWebElement SearchPassword = Browser.FindElement(By.Name("password"));
-            SearchPassword.SendKeys("Michael2013" + OpenQA.Selenium.Keys.Enter);
-            //Random delay 5-9 sec
-            Random time1 = new Random();
-            int t1 = time1.Next(5000, 9000);
-            System.Threading.Thread.Sleep(t1);
+            SearchPassword.SendKeys("7394Piter" + OpenQA.Selenium.Keys.Enter);
+            Delay(5000, 9000);
 
-            var  te = Browser.Url;
-            if (true)
+            currentUrl = Browser.Url;
+            if (currentUrl == "https://www.instagram.com/#reactivated")
             {
                 IWebElement seachDontNow = Browser.FindElement(By.LinkText("Не сейчас"));
                 seachDontNow.Click();
-                Random time3 = new Random();
-                int t3 = time3.Next(3000, 9000);
-                System.Threading.Thread.Sleep(t3);
+                Delay(3000, 9000);
             }
-            //find element explore,click 
-            IWebElement SearchExplore = Browser.FindElement(By.LinkText("Найти людей"));
-            SearchExplore.Click();
+            //go tag element explore,click 
+            Browser.Navigate().GoToUrl("https://www.instagram.com/explore/tags/триатлон/");
+           
+            //IWebElement SearchExplore = Browser.FindElement(By.LinkText("Найти людей"));
+            //SearchExplore.Click();
             //Random delay 5-9 sec
             Random time2 = new Random();
             int t2 = time2.Next(3000, 9000);
@@ -52,60 +65,64 @@ namespace InstaBot
             //Search,choose images rand of 6
             List<IWebElement> SearchImages = Browser.FindElements(By.XPath("//div[@class='_havey']/div/div/a")).ToList();
             Random imgChange = new Random();
-            int iC = imgChange.Next(0,5);
+            int iC = imgChange.Next(0, 5);
             SearchImages[iC].Click();
-            //Random delay 5-35 sec
-            Random delayLike = new Random();
-            int dL = delayLike.Next(5000,35000);
-            System.Threading.Thread.Sleep(dL);
+            logger.Info("first like");
+            Delay(5000, 35000);
+            Liker();
+        }
+        public void Liker()
+        {
+            logger.Info("start Liker");
+            int summLikes = 0;
+            Random likeRandom = new Random();
+            int lR = likeRandom.Next(20, 30);
+            try
+            {
 
-            Like();
-            
-            
-            //restriction likes 10-15
-            Random restrictionLikeHour = new Random();
-            int rLH = restrictionLikeHour.Next(10, 15);
-            for (int i = 0; i < rLH; i++)
+                for (int i = 0; i < 160; i++)//159 review max
+                {
+                    if (summLikes < 30)
+                    {
+                        //probability = 40 %
+                        Random r = new Random();
+                        var p = r.Next(1, 10);
+                        if (p < 5)
+                        {
+                            //Seach button "like",click
+                            List<IWebElement> SearchLike = Browser.FindElements(By.XPath("//article//div/section/a")).ToList();
+                            SearchLike[0].Click();
+                            Delay(2000, 5000);
+                            summLikes++;
+                            logger.Info("summLikes = {0}", summLikes);
+                        }
+                        else
+                        {
+                            logger.Info("not like = ", summLikes);
+                        }
+                        IWebElement SearchNextImg = Browser.FindElement(By.LinkText("Далее"));
+                        SearchNextImg.Click();
+                        Delay(7000, 22000);
+                    }
+                    else
+                    {
+                        i = 160;
+                        logger.Info("Liker finish");
+                    }
+                }
+                
+            }
+            catch (Exception exc)
             {
-                //Click next image
-                //amount likes
-                //Random 1-10 for decision
-                Random decision = new Random();
-                int d = decision.Next(1, 10);
-                if (d < 3)
-                {
-                    //like
-                }
-                else
-                {
-                    //nextimg
-                }
+                logger.Error(exc.Message);
+                throw;
             }
         }
-        public void Like()
+        public void Delay(int Time1, int Time2)
         {
-            //like<100 and probability =30%
-            if (true)
-            {
-                //Seach button "like",click
-                List<IWebElement> SearchLike = Browser.FindElements(By.XPath("//article//div/section/a")).ToList();
-                SearchLike[0].Click();
-                //Random 2-5 sec
-                Random timeNext = new Random();
-                int tN = timeNext.Next(2000, 5000);
-                System.Threading.Thread.Sleep(tN);
-                Next();
-            }
-        }
-        //
-        public void Next()
-        {
-            IWebElement SearchNextImg = Browser.FindElement(By.LinkText("Далее"));
-            SearchNextImg.Click();
-            //Random 7-22 sec
-            Random timeNext = new Random();
-            int tN = timeNext.Next(7000, 22000);
-            System.Threading.Thread.Sleep(tN);
-        }
+            Random timeRandom = new Random();
+            int tR = timeRandom.Next(Time1, Time2);
+            System.Threading.Thread.Sleep(tR);
+        }                                                       //Delay vs filter insta bots
     }
 }
