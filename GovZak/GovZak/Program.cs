dcbbb6531c02d0cb24aa29f8b2c5ba3eb27e58da
MMZhirkov <mmzhirkov@yandex.ru>
2018-05-08 11:08:56 +0300

    add gov zakup correct instabot

---
+++ GovZak/GovZak/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace GovZak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentUrl;
+            string parRegion;
+            string parOkpd2Ids;
+            string parOkpd2IdsCodes;
+            string urlStr = "http://zakupki.gov.ru/epz/order/extendedsearch/results.html?morphology=on&openMode=DEFAULT_SAVED_SETTING&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&fz44=on&placingWaysList=EF&pc=on&priceTo=1000000&currencyId=-1&publishDateFrom=31.12.2016&publishDateTo=01.01.2018&region_deliveryRegionIds_5277352=region_deliveryRegionIds_5277352&deliveryRegionIds=5277352&regionDeleted=false&okpdIds=3291&okpdIdsCodes=01.11.1&sortBy=UPDATE_DATE";
+
+           
+
+
+            string jsonData = System.IO.File.ReadAllText(@"Content\DictForGovZakup.json");
+            GovZakup jsonP = JsonConvert.DeserializeObject<GovZakup>(jsonData);
+            IWebDriver Browser;
+            
+            
+
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser.Navigate().GoToUrl(urlStr);
+        }
+    }
+    public class GovZakup {
+        [JsonProperty("OKPO2")]
+        public string okpo2 { get; set; }
+        [JsonProperty("Region")]
+        public string Region { get; set; }
+    }
+    public class Href
+    {
+        [JsonProperty("OKPO2")]
+        public string Okpo2 { get; set; }
+        [JsonProperty("Region")]
+        public string Region { get; set; }
+        [JsonProperty("Path")]
+        public string Path { get; set; }
+    }
+}
---
+++ InstaBot/InstaBot/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using NLog; 
+using NLog.Config;
+using System.Threading;
+
+
+namespace InstaBot
+{
+    public partial class Form1 : Form
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        List<string> Tags = new List<string>();
+        int likes = 0;
+
+
+        IWebDriver Browser;
+        public Form1()
+        {
+            InitializeComponent();
+            Login();
+            Tags.Add("2");
+            //revision
+            //1 work for tag
+            //2 like menee 200
+            //3 video prosmotr
+        }
+        public void Login() {
+            string currentUrl;
+            //pass authorization
+            logger.Info("login");
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser.Navigate().GoToUrl("https://www.instagram.com/accounts/login/");
+            Delay(2000, 4000);
+            IWebElement SearchLogin = Browser.FindElement(By.Name("username"));
+            SearchLogin.SendKeys("mmzhirkov@yandex.ru");
+            IWebElement SearchPassword = Browser.FindElement(By.Name("password"));
+            SearchPassword.SendKeys("7394Piter" + OpenQA.Selenium.Keys.Enter);
+            Delay(5000, 9000);
+
+            currentUrl = Browser.Url;
+            if (currentUrl == "https://www.instagram.com/#reactivated")
+            {
+                IWebElement seachDontNow = Browser.FindElement(By.LinkText("Не сейчас"));
+                seachDontNow.Click();
+                Delay(3000, 9000);
+            }
+            //Enter Tag in finder
+            IWebElement SearchFinder = Browser.FindElement(By.XPath("//section/nav/div[2]/div/div/div[2]/input"));
+            SearchFinder.SendKeys("#бег");//мясомясо шашлыки  кроссфит  триатлон, , triathlon ,  meat,wine , football  , футбол ,running ,steak , , , , , , ,  
+                                       //   мясомясо      стейк  мясо      run   шашлыки   running  говядина вино бегун crossfit бег
+            Delay(2000, 3000);
+            IWebElement SearchFirstTag = Browser.FindElement(By.XPath("//section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[1]"));
+            //*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[1]
+
+            SearchFirstTag.Click();
+            Delay(3000, 5000);
+
+            //Search,choose first images rand of 10-12
+            IWebElement SearchImages = Browser.FindElement(By.XPath("//section/main/article/div[2]/div/div[1]/div[1]/a"));
+            Random imgChange = new Random();
+            int iC = imgChange.Next(9, 12);
+            SearchImages.Click();
+            logger.Info("first img opened");
+
+            Delay(5000, 9000);
+            Liker();
+        }
+        public void Liker()
+        {
+            logger.Info("start Liker");
+
+            if (likes < 100)
+            {
+                IWebElement countLike = null;
+                try
+                {
+                     countLike = Browser.FindElement(By.XPath("//section[2]/div/a/span"));
+                }
+                catch (Exception exc)
+                {
+
+                    logger.Error("Error -", exc);
+                }
+
+                if (countLike==null)
+                    {
+                    logger.Info("not like = ", likes);
+                    IWebElement SearchNextImg1 = Browser.FindElement(By.LinkText("Далее"));
+                    SearchNextImg1.Click();
+                    Delay(2000, 5000);
+                    Liker();
+                }
+                else
+                    {
+                        if (int.Parse(countLike.Text.Replace(" ","")) > 199)
+                            {
+                                IWebElement SearchNextImg1 = Browser.FindElement(By.LinkText("Далее"));
+                                SearchNextImg1.Click();
+                                Liker();
+                            }
+                        else if (int.Parse(countLike.Text) < 200)
+                            {
+                                //probability = 40 %
+                                Random r = new Random();
+                                var p = r.Next(1, 10);
+                                if (p < 5)
+                                {
+                                    //Seach button "like",click
+                                    IWebElement SearchLike = Browser.FindElement(By.XPath("//article//div/section/a"));
+                                    SearchLike.Click();
+                                    likes++;
+                                    logger.Info("summLikes = {0}", likes);
+                                    Delay(2000, 5000);
+                                    Liker();
+                                }
+                                else
+                                {
+
+                            //если нету далее пропиши
+                                    logger.Info("not like = ", likes);
+                                    IWebElement SearchNextImg1 = Browser.FindElement(By.LinkText("Далее"));
+                                    SearchNextImg1.Click();
+                                    Delay(2000, 5000);
+                                    Liker();
+                                }
+                            }
+                        else
+                            {
+                                var urlError = Browser.Url;
+                                logger.Error("start Liker", urlError);
+                            }
+                }
+            }
+            else
+            {
+                logger.Info("Exit app");
+                Application.Exit();
+            }
+        }
+        public void Delay(int Time1, int Time2)
+        {
+            Random timeRandom = new Random();
+            int tR = timeRandom.Next(Time1, Time2);
+            System.Threading.Thread.Sleep(tR);
+        }                                                       //Delay vs filter insta bots
+    }
+}
